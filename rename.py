2af8f740ed38936abd67733c1a42bf7966935297
AceBurgundy <samadriansabalo99@gmail.com>
2022-07-12 17:15:48 +0800

    wrote the choose function allowing use to navigate through the available options and also wrote the function names of the CLI features

--- rename.py
+++ rename.py
@@ -1,5 +1,9 @@
 import re
+import os
 
+def clear():
+    os.system("cls")
+    
 def fixPathName(location):
     pattern = re.compile(r'\s+')
     if "\\" in location:
@@ -7,7 +11,32 @@ def fixPathName(location):
         return folder_directory
     else:
         print("    this is not a directory")
-        
+
+def choose(location):
+    clear()
+    os.chdir(location)
+    folders = os.listdir(location)
+    
+    print("""\nChoose from the following options
+          
+              Remove unnecessary details like: [English] [1080] 480p Engsub         press 1
+              Add a number to sequence all your files                               press 2
+              Remove all symbols from the folders name                              press 3
+              Default (remove unnecessary details and sequence all folders)         press 4\n""")
+    
+    option = int(input("    Choose: "))
+    
+    if option == 1:
+        remove_words(folders),
+    elif option == 2:
+        incrementFolders(folders),
+    elif option == 3:
+        remove_symbols(folders),
+    elif option == 4:
+        basic(folders),
+    else:
+        print("    Invalid input")
+              
         
 def main():
     location = fixPathName(input("""
